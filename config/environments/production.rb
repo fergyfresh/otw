263c4e757b1c8391bf77873ec6fb36473966ecca
Billy <william.d.ferg@gmail.com>
2017-02-20 17:33:49 -0800

    Add in ssl middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+	config.force_ssl = true
 	config.middleware.use Letsencrypt::Middleware
 	config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 
