80f5158ec0e731c03f05e71c2dc67c809ffc92c4
Billy Ferguson <william.d.ferg@gmail.com>
2017-02-24 04:27:25 -0800

    Update cable.js

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -4,10 +4,5 @@
 //= require action_cable
 //= require_self
 //= require_tree ./channels
-
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
+this.App || (this.App = {});
+App.cable = ActionCable.createConsumer();
