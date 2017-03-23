c8893fc26ca441ffbd770c33270f593411cc2a95
Billy Ferguson <william.d.ferg@gmail.com>
2017-03-23 13:13:35 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :messages
-	has_many :groupchats, through: :messages
-			
+  has_many :groupchats, through: :messages		
+
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable,
-				 :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook]
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
@@ -12,21 +13,21 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
-	end
+  end
 
-	def password_required?
-  	super && self.provider.blank?
-	end
+  def password_required?
+    super && self.provider.blank?
+  end
 
-	def update_with_password(params, *options)
-  	if encrypted_password.blank?
-    	update_attributes(params, *options)
-  	else
-    	super
-  	end
-	end
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+     super
+    end
+  end
 
-	def has_no_password?
-  	self.encrypted_password.blank?
-	end
+  def has_no_password?
+    self.encrypted_password.blank?
+  end
 end
