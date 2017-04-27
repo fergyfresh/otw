29bd16428afa4a480501f9bbd384c2b883e8b0dd
Billy Ferguson <william.d.ferg@gmail.com>
2017-04-27 15:20:46 -0400

    Update messages.js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,15 +1,21 @@
 $(document).on('turbolinks:load', function() {
-	groupchatId = $('input#message_groupchat_id').val();
-	App.messages = App.cable.subscriptions.create({channel: 'MessagesChannel', groupchat_id: groupchatId}, {
-		received: function(data) {
-			$("#messages").removeClass('hidden');
-			return $('#messages').append(this.renderMessage(data));
-		},
-		groupchat_id: function(data) {
-			return data.groupchat_id
-		},
-		renderMessage: function(data) {
-			return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
-		}
-	});
+  groupchatId = $('input#message_groupchat_id').val();
+  locations = {}     
+  App.messages = App.cable.subscriptions.create({channel: 'MessagesChannel', groupchat_id: groupchatId}, {
+    received: function(data) {
+      if (data.message.includes("Latitude:")) {
+        locations[data.user] = data.message.split(";")[0];
+        return;
+      } else {
+        $("#messages").removeClass('hidden');
+        return $('#messages').append(this.renderMessage(data));
+      }
+    },
+    groupchat_id: function(data) {
+      return data.groupchat_id
+    },
+    renderMessage: function(data) {
+      return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    }
+  });
 })
