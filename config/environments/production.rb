a12f7c88c5f9a9dcf4dc97375c55406e6667ce98
Billy Ferguson <william.d.ferg@gmail.com>
2017-05-04 09:16:53 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-	config.force_ssl = true
-	config.middleware.use Letsencrypt::Middleware
-	config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
+  config.force_ssl = true
+  config.middleware.use Letsencrypt::Middleware
+  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
