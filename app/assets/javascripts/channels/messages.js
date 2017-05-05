4d62c57d8ad0942b7e90a505e44e9e3c51a1ce23
Billy Ferguson <william.d.ferg@gmail.com>
2017-05-05 10:10:10 -0400

    Update messages.js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
   groupchatId = $('input#message_groupchat_id').val();
-  locations = {}
+  locations = {};
   var map = L.map('map').setView([41.4000000, -72.1000000], 13);
   // load a tile layer
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
