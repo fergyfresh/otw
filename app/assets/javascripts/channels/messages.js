bd0ab5d99c5d8e4af1832134c3e9ebf1ad2b52a2
Billy <william.d.ferg@gmail.com>
2017-05-07 14:22:47 -0700

    Fixed framing issue

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function() {
   groupchatId = $('input#message_groupchat_id').val();
   locations = {};
   var map = L.map('map').setView([0, 0], 13);
+  var bounds = new L.LatLngBounds();
   // load a tile layer
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
@@ -10,12 +11,15 @@ $(document).on('turbolinks:load', function() {
   
   App.messages = App.cable.subscriptions.create({channel: 'MessagesChannel', groupchat_id: groupchatId}, {
     received: function(data) {
+      var latlng = data.message.split(";").slice(1, 3);
       if (data.message.includes("LatitudeLongitude;")) {
         if (locations[data.user] === undefined) {
-          locations[data.user] = L.marker(data.message.split(";").slice(1, 3)).addTo(map).bindPopup(data.user);
+          locations[data.user] = L.marker(latlng).addTo(map).bindPopup(data.user);
         } else {
-          locations[data.user].setLatLng(data.message.split(";").slice(1, 3));
+          locations[data.user].setLatLng(latlng).update();
         }
+        bounds.extend(latlng);
+        map.fitBounds(bounds, {padding:[50,50]});
         return;
       } else {
         $("#messages").removeClass('hidden');
