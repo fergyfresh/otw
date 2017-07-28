58b9d4196b28f34af068bb27b8b2b2912dc6bb34
Billy Ferguson <william.d.ferg@gmail.com>
2017-07-28 13:22:18 -0400

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,8 @@
 module Api::V1
   class UsersController < ApiController
-
-		# GET /v1/users
-	  def index
- 		  render json: User.all
- 	 	end
-
+    # GET /v1/users
+    def index
+      render json: User.all
+    end
   end
 end
