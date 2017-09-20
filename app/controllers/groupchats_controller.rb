9ccd0ee2c541c243b4ae728cea975aa8aa671f5f
Billy Ferguson <william.d.ferg@gmail.com>
2017-09-20 14:18:52 -0400

    Update groupchats_controller.rb

--- app/controllers/groupchats_controller.rb
+++ app/controllers/groupchats_controller.rb
@@ -15,6 +15,7 @@ class GroupchatsController < ApplicationController
   end
 
   def create
+    groupchat_params[:user_ids] = groupchat_params[:userids][0].split(',')
     @groupchat = Groupchat.new(groupchat_params)
     @groupchat.users << current_user
 
