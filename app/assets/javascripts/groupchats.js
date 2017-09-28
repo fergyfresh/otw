8382b202267b92c12c90f111b120060afb1d955f
Billy Ferguson <william.d.ferg@gmail.com>
2017-09-28 14:42:55 -0400

    Update groupchats.js

--- app/assets/javascripts/groupchats.js
+++ app/assets/javascripts/groupchats.js
@@ -1,14 +1,18 @@
 $(document).on('turbolinks:load', function() {
   submitNewMessage();
-  $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  $('#messages').scrollTop($('#messages')[0].scrollHeight);
 });
 
 function submitNewMessage(){
   $('textarea#message_content').keydown(function(event) {
     if (event.keyCode == 13) {
         $('[data-send="message"]').click();
-        $('[data-textarea="message"]').val("")
+        $('[data-textarea="message"]').val("");
         return false;
      }
+  });
+  $('input.send_icon').click(function() {
+    $('[data-send="message"]').click();
+    $('[data-textarea="message"]').val("");
   });
 }
