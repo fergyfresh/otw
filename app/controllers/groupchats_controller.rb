fad94a4ab452bdd063c4877da967bc1854565cdf
Billy Ferguson <william.d.ferg@gmail.com>
2017-10-11 09:26:13 -0400

    Update groupchats_controller.rb

--- app/controllers/groupchats_controller.rb
+++ app/controllers/groupchats_controller.rb
@@ -18,7 +18,7 @@ class GroupchatsController < ApplicationController
   def destroy
     @groupchat = Groupchat.find_by(slug: params[:slug])
     if @groupchat.users[0] == current_user
-      @groupchat.destroy
+      @groupchat.destroy_all
     end
     redirect_to authenticated_root_url
   end    
